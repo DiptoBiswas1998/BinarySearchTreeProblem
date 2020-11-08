d487be7f28229961155be07f69d7a5525e8b9e2e
unknown <diptobiswas1998@gmail.com>
2020-11-08 22:07:38 +0530

    [Dipto] Add: Creating and displaying a Binary Search Tree

--- BSTProblem/BSTProblem/Program.cs
+++ BSTProblem/BSTProblem/Program.cs
@@ -6,6 +6,11 @@ namespace BSTProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Binary Search Tree Problem!");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            Console.WriteLine("Binary Search Tree: ");
+            binarySearchTree.Display();
         }
     }
 }
